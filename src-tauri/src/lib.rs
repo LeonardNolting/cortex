84649268060490d419ccccffb14732b6d9e3cd90
Leonard Nolting <leonolting@gmail.com>
2026-07-16 21:14:10 +0200

    add scoped path access configuration plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -20,6 +20,7 @@ pub fn run() {
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_persisted_scope::init())
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet, fetch_jveg])
